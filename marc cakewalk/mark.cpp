27f0fe74108d5f30f8a1278e0769d561fcdb4563
samyak360 <samyak360@gmail.com>
2019-02-14 00:05:19 +0530

    added mark cakewalk

---
+++ marc cakewalk/mark.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+
+int main (){
+    int n;
+    cin >> n;
+    int A[n];
+    int sum=0;
+    for(int i=0;i<n;i++){
+        cin >> A[i];
+    }
+    sort(A,A+n,greater<int>());
+    for(int i =0 ;i<n;i++){
+        sum+=A[i]*pow(2,i);
+    }
+
+    cout << int(sum);
+}
